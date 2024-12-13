8c8682b24f31d5199271cce7ea7d718805ec8415
ComfortAtobrah <atobrahcomfort88@gmail.com>
2024-12-13 12:45:12 +0000

    first commit

---
+++ Homework6/src/index.js
@@ -0,0 +1,36 @@
+homework6
+let name= "Comfort";
+console.log(name);
+
+let age="19";
+console.log(age)
+
+let num1 = 13;
+let num2 = 7;
+console.log(num1 + num2);
+
+let num3 = 20;
+let num4 = 1;
+console.log(num3 - num4);
+
+let num5 = 44;
+let num6 = 2;
+console.log(num5 / num6);
+
+
+let num7 = 90;
+let num8 = 4;
+console.log(num7 * num8);
+
+let num9 = 15;
+let num10 = 4;
+console.log(num9 % num10);
+
+let rain = true;
+console.log(rain);
+
+let yam = 12;
+console.log(typeof(yam));
+
+let baby = "Atobrah ";
+console.log(typeof(baby));
